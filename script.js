3be6185244e77a1dd35dd65fb5e649070a2f5c86
Edafe Emmanuel <edafeemmanuel@Edafes-MacBook-Pro.local>
2025-08-14 00:37:24 +0100

    carousel

--- script.js
+++ script.js
@@ -11,3 +11,48 @@ headers.forEach((header) => {
     content.classList.toggle("active");
   });
 });
+
+//========== Carousel Code ===========//
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+const track = document.querySelector(".carousel-track");
+const slides = document.querySelectorAll(".carousel-slide img");
+
+let currentIndex = 0;
+
+next.addEventListener("click", () => {
+  if (currentIndex < slides.length - 1) {
+    currentIndex++;
+  } else currentIndex = 0;
+  track.style.transform = `translateX(${-currentIndex * 600}px)`;
+});
+
+prev.addEventListener("click", () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+  } else currentIndex = slides.length - 1;
+  track.style.transform = `translateX(${-currentIndex * 600}px)`;
+});
+
+//========== Slider Code ===========//
+
+const sliderTracker = document.querySelector(".slider-tracker");
+const nextBtn = document.querySelector(".nextBtn");
+const prevBtn = document.querySelector(".prevBtn");
+const images = document.querySelectorAll(".slider-slide img");
+
+let currentIndice = 0;
+
+nextBtn.addEventListener("click", () => {
+  if (currentIndice < images.length - 1) {
+    currentIndice++;
+  } else currentIndice = 0;
+  sliderTracker.style.transform = `translateY(${-currentIndice * 300}px)`;
+});
+
+prevBtn.addEventListener("click", () => {
+  if (currentIndice > 0) {
+    currentIndice--;
+  } else currentIndice = images.length - 1;
+  sliderTracker.style.transform = `translateY(${-currentIndice * 300}px)`;
+});
